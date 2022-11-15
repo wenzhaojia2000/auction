8e535b2c38b29f3f4ceec4c19fea7b9f2cbe61ff
cammillen <116878680+cammillen@users.noreply.github.com>
2022-11-15 12:12:53 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
   // For now, index.php just redirects to browse.php, but you can change this
   // if you like.
+
+  try {
+    $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=cameronmillen;user=postgres;password=Mullingar1609!');
+    echo "PDO connection object created";
+  }
+  catch(PDOException $e) {
+    echo $e->getMessage();
+  }
   
   header("Location: browse.php");
 ?>
