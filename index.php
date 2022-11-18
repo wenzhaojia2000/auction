f62e92729b3f560cf1b3a12365e3330c860c95c0
cammillen <cameronrmillen@gmail.com>
2022-11-18 16:21:28 +0000

    lol

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
   // For now, index.php just redirects to browse.php, but you can change this
   // if you like.
-
   try {
-    $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=cameronmillen;user=postgres;password=Mullingar1609!');
+    $dbh = new PDO('pgsql:host=localhost;port=5432;dbname=cameronmillen;user=postgres;password=');
     echo "PDO connection object created";
   }
   catch(PDOException $e) {
