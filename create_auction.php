68a73ae9f9849bf5f8f583f29a160c0c009b502a
James Jia <32031865+wenzhaojia2000@users.noreply.github.com>
2022-11-22 14:53:32 +0000

    Adds names to create_auction.php

--- create_auction.php
+++ create_auction.php
@@ -28,21 +28,21 @@
         <div class="form-group row">
           <label for="auctionTitle" class="col-sm-2 col-form-label text-right">Title of auction</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" id="auctionTitle" placeholder="e.g. Black mountain bike">
+            <input type="text" class="form-control" id="auctionTitle" name="itemName" placeholder="e.g. Black mountain bike">
             <small id="titleHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> A short description of the item you're selling, which will display in listings.</small>
           </div>
         </div>
         <div class="form-group row">
           <label for="auctionDetails" class="col-sm-2 col-form-label text-right">Description</label>
           <div class="col-sm-10">
-            <textarea class="form-control" id="auctionDetails" rows="4"></textarea>
+            <textarea class="form-control" id="auctionDetails" name="itemDescription" rows="4"></textarea>
             <small id="detailsHelp" class="form-text text-muted">Full details of the listing to help bidders decide if it's what they're looking for.</small>
           </div>
         </div>
         <div class="form-group row">
           <label for="auctionCategory" class="col-sm-2 col-form-label text-right">Category</label>
           <div class="col-sm-10">
-            <select class="form-control" id="auctionCategory">
+            <select class="form-control" id="auctionCategory" name="category">
               <option selected>Choose...</option>
               <option value="fill">Fill me in</option>
               <option value="with">with options</option>
@@ -54,7 +54,7 @@
         <div class="form-group row">
           <label for="itemCondition" class="col-sm-2 col-form-label text-right">Condition</label>
           <div class="col-sm-10">
-            <select class="form-control" id="itemCondition">
+            <select class="form-control" id="itemCondition" name="itemCondition">
               <option selected>Choose...</option>
               <option value="new">New</option>
               <option value="used">Used</option>
@@ -66,7 +66,7 @@
         <div class="form-group row">
           <label for="auctionCategory" class="col-sm-2 col-form-label text-right">Image(s)</label>
           <div class="col-sm-10">
-            <input class="form-control" id="itemImage" type="file" name="files[]" multiple/>
+            <input class="form-control" id="itemImage" type="file" name="itemImage[]" multiple/>
             <small id="imageImageHelp" class="form-text text-muted">Choose one or more images of the item. Supported image formats: .jpeg, .jpg, .png, .gif</small>
           </div>
         </div>
@@ -77,7 +77,7 @@
               <div class="input-group-prepend">
                 <span class="input-group-text">£</span>
               </div>
-              <input type="number" class="form-control" id="auctionStartPrice">
+              <input type="number" class="form-control" id="auctionStartPrice" name="startingPrice">
             </div>
             <small id="startBidHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Initial bid amount.</small>
           </div>
@@ -89,7 +89,7 @@
               <div class="input-group-prepend">
                 <span class="input-group-text">£</span>
               </div>
-              <input type="number" class="form-control" id="auctionReservePrice">
+              <input type="number" class="form-control" id="auctionReservePrice" name="reservePrice">
             </div>
             <small id="reservePriceHelp" class="form-text text-muted">Optional. Auctions that end below this price will not go through. This value is not displayed in the auction listing.</small>
           </div>
@@ -115,7 +115,7 @@
         <div class="form-group row">
           <label for="auctionEndDate" class="col-sm-2 col-form-label text-right">End date</label>
           <div class="col-sm-10">
-            <input type="datetime-local" class="form-control" id="auctionEndDate">
+            <input type="datetime-local" class="form-control" id="auctionEndDate" name="endDate">
             <small id="endDateHelp" class="form-text text-muted"><span class="text-danger">* Required.</span> Day for the auction to end.</small>
           </div>
         </div>
