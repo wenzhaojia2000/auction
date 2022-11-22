7eedeec135d8fc59b0b50502324acb45f3644b9a
James Jia <32031865+wenzhaojia2000@users.noreply.github.com>
2022-11-22 11:45:56 +0000

    Adds name to register.php, adds sample php to auction result

--- create_auction_result.php
+++ create_auction_result.php
@@ -16,6 +16,29 @@
             make sure it can be inserted into the database. If there is an
             issue, give some semi-helpful feedback to user. */
 
+// Uploads files locally to a directory "images/". Doesn't add to database yet.
+// From https://stackoverflow.com/questions/24895170/multiple-image-upload-php-form-with-one-input
+$error=array();
+$extension=array("jpeg","jpg","png","gif");
+foreach($_FILES["files"]["tmp_name"] as $key=>$tmp_name) {
+      $file_name=$_FILES["files"]["name"][$key];
+      $file_tmp=$_FILES["files"]["tmp_name"][$key];
+      $ext=pathinfo($file_name,PATHINFO_EXTENSION);
+
+      if(in_array($ext,$extension)) {
+         if(!file_exists("images/".$txtGalleryName."/".$file_name)) {
+            move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"images/".$txtGalleryName."/".$file_name);
+         }
+         else {
+            $filename=basename($file_name,$ext);
+            $newFileName=$filename.time().".".$ext;
+            move_uploaded_file($file_tmp=$_FILES["files"]["tmp_name"][$key],"images/".$txtGalleryName."/".$newFileName);
+         }
+      }
+      else {
+         array_push($error,"$file_name, ");
+      }
+}
 
 /* TODO #3: If everything looks good, make the appropriate call to insert
             data into the database. */
